2349c9af47a993d2fe1dfda3970d4baadca48e94
Hwangdawon5 <yiouech124@naver.com>
2025-08-13 15:37:16 +0900

    Delete asdjf.py

--- asdjf.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-cred = credentials.Certificate("twenty-864c6-firebase-adminsdk-fbsvc-254d40e95d.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-db.collection("healthcheck").document("ping").set({"ts": firestore.SERVER_TIMESTAMP})
-print("OK")
